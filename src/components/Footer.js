16de2e7c0505f791df6806cd7fb9305beb925565
iamEJ <jukneviciute185@gmail.com>
2021-04-27 19:34:54 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,61 @@
+import styled from "styled-components";
+import { AiOutlineFacebook } from "react-icons/ai";
+
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <Wrapper>
+      <a href="https://www.facebook.com/origamidovanos">
+        Follow Us: <AiOutlineFacebook />
+      </a>
+      <div className="footer-copy">
+        <p>
+          &copy; {new Date().getFullYear()}
+          <span> Origami Gift </span>
+        </p>
+        <p>| All rights reserved.</p>
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  heigth: 80px;
+  width: 90vw;
+  margin: 0 auto;
+  border-top: 1px solid #d9d9d9;
+  padding: 0 20px;
+
+  .footer-copy {
+    display: flex;
+    p {
+      font-size: 12px;
+      color: #4d4d4d;
+      span {
+        margin-right: 3px;
+        color: #72abf2;
+        font-weight: 500;
+      }
+    }
+  }
+
+  a {
+    display: flex;
+    color: #4d4d4d;
+    font-size: 12px;
+    align-items: center;
+    padding: 20px;
+    text-decoration: none;
+    svg {
+      font-size: 24px;
+      margin-left: 5px;
+      &:hover {
+        color: #72abf2;
+      }
+    }
+  }
+`;
 
 export default Footer;
